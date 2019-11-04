d8c3cb50383de5d2fca05fc05582e70b4d6d6be7
FoxFite-Wu <861150820@qq.com>
2019-11-04 19:50:04 +0800

    h2 commit

--- src/main/java/com/wcb/community_1/controller/AuthorizeController.java
+++ src/main/java/com/wcb/community_1/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,13 +37,14 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(Redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
-        //System.out.println(accessToken);
         GitHubUser user = gitHubProvider.getUser(accessToken);
-        //System.out.println(user);
-        System.out.println(user.getName());
-        System.out.println(user.getId());
-        System.out.println(user.getBio());
 
-        return "index";
+        if (user != null){
+              request.getSession().setAttribute("user",user);
+              return "redirect:/";
+        }else{
+          return "redirect:/";
+        }
+
     }
 }
