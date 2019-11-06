fc8a72264e60462ff77a8e3b291c7348137b5748
FoxFite-Wu <861150820@qq.com>
2019-11-06 18:51:26 +0800

    cookie commit

--- src/main/java/com/wcb/community_1/controller/AuthorizeController.java
+++ src/main/java/com/wcb/community_1/controller/AuthorizeController.java
@@ -1,5 +1,6 @@
 package com.wcb.community_1.controller;
 
+import com.sun.deploy.net.HttpResponse;
 import com.wcb.community_1.dto.AccessTokenDTO;
 import com.wcb.community_1.dto.GitHubUser;
 import com.wcb.community_1.mapper.UserMapper;
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +38,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -47,13 +50,14 @@ public class AuthorizeController {
         System.out.println(gitHubUser.getName());
         if (gitHubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitHubUser.getName());
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",gitHubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
           return "redirect:/";
--- src/main/java/com/wcb/community_1/mapper/UserMapper.java
+++ src/main/java/com/wcb/community_1/mapper/UserMapper.java
@@ -3,9 +3,15 @@ package com.wcb.community_1.mapper;
 import com.wcb.community_1.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
-    }
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
